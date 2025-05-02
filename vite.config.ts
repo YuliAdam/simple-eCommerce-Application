543e14a1a8901a6f5235c8d506d94a0c8c4af168
madsok <madsok25@gmail.com>
2025-05-02 23:01:46 +0300

    chore: add main alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'node:path';
+
+export const pathResolver = (p: string) => resolve(__dirname, '.', p);
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': pathResolver('./src'),
+    },
+  },
 });
