608834b88fddcc7579ab3c781981182257d3ea50
YuliAdam <ylia_adamovich@mail.ru>
2025-05-09 16:23:07 +0200

    refactor: change import, remove console.log

--- src/services/client.ts
+++ src/services/client.ts
@@ -1,7 +1,7 @@
 import { ctpClient } from './BuildClient';
 import type { ClientResponse, Project } from '@commercetools/platform-sdk';
 import { createApiBuilderFromCtpClient } from '@commercetools/platform-sdk';
-import { projectKey } from './constantData';
+import { projectKey } from './apiEnv.ts';
 
 // Create apiRoot from the imported ClientBuilder and include your Project key
 export const apiRoot = createApiBuilderFromCtpClient(ctpClient).withProjectKey({
@@ -11,7 +11,5 @@ export const apiRoot = createApiBuilderFromCtpClient(ctpClient).withProjectKey({
 // Example call to return Project information
 // This code has the same effect as sending a GET request to the commercetools Composable Commerce API without any endpoints.
 export const getProject = (): Promise<ClientResponse<Project>> => {
-  console.log(apiRoot);
-  console.log(ctpClient);
   return apiRoot.get().execute();
 };
