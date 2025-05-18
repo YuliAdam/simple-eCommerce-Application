830ab56f51746b662464d42742d28c3b7e5d4d73
wingedseraph <r89647@gmail.com>
2025-05-18 13:10:48 +0300

    fix: change magic string to constant of localStorageConfig

--- src/pages/user/user.tsx
+++ src/pages/user/user.tsx
@@ -1,7 +1,8 @@
+import { shop } from '@/config/localStorageConfig';
 import type { JSX } from 'react';
 
 export function User(): JSX.Element {
   //  impl getUser() >  from api
-  const userClientId = localStorage.getItem('[simple]client_id');
+  const userClientId = localStorage.getItem(shop.client_id);
   return <section>user: {userClientId}</section>;
 }
