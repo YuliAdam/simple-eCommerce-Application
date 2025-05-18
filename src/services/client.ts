8723ba9b6be9d29bad7346f98e5e842fc2773490
madsok <madsok25@gmail.com>
2025-05-18 13:23:06 +0300

    feat: add utility for apiRoot creation

--- src/services/client.ts
+++ src/services/client.ts
@@ -1,7 +1,8 @@
-import { ctpClient } from './BuildClient';
+import { ctpClient } from '@/services/BuildClient.ts';
 import type { ClientResponse, Project } from '@commercetools/platform-sdk';
 import { createApiBuilderFromCtpClient } from '@commercetools/platform-sdk';
 import { projectKey } from './apiEnv.ts';
+import type { Client } from '@commercetools/ts-client';
 
 // Create apiRoot from the imported ClientBuilder and include your Project key
 export const apiRoot = createApiBuilderFromCtpClient(ctpClient).withProjectKey({
@@ -13,3 +14,11 @@ export const apiRoot = createApiBuilderFromCtpClient(ctpClient).withProjectKey({
 export const getProject = (): Promise<ClientResponse<Project>> => {
   return apiRoot.get().execute();
 };
+
+// Утилита для создания клиента. Передаем при вызове функции настройки, например, ctpClient из BuildClient.ts и дальше используем apiRoot.
+export function setApiRoot(client: Client) {
+  const apiRoot = createApiBuilderFromCtpClient(client).withProjectKey({
+    projectKey: projectKey,
+  });
+  return apiRoot;
+}
