ccecaf23671b13712958cf1c88c736d953e030b9
madsok <madsok25@gmail.com>
2025-05-21 22:09:00 +0300

    fix: fix linter warn

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -40,7 +40,7 @@ export function Header() {
     if (authToken) {
       dispatch(login(authToken));
     }
-  }, []);
+  }, [dispatch]);
 
   function handleMenu() {
     setIsOpenMenu(state => !state);
