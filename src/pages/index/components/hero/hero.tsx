6fd23c7880aa9d4eb383609b06b6f95bffd1f099
wingedseraph <r89647@gmail.com>
2025-06-05 17:15:52 +0300

    feat: update index page to have promocode's content

--- src/pages/index/components/hero/hero.tsx
+++ src/pages/index/components/hero/hero.tsx
@@ -5,27 +5,32 @@ import { Path } from '@/config/routesConfig';
 export default function Hero() {
   return (
     <section className={styles.hero}>
-      <div className={styles.container}>
-        <div className={styles.content}>
-          <h1 className={styles.title}>
-            simple
-            <br />
-            clothes
-          </h1>
-          <Link to={Path.allProducts} className={styles.subtitle}>
-            &gt; let's buy something
-          </Link>
+      <div className={styles.content}>
+        <h1 className={styles.title}>
+          simple
+          <br />
+          clothes
+        </h1>
+        <Link to={Path.allProducts} className={styles.subtitle}>
+          &gt; let's buy something
+        </Link>
+        <div className={styles.promocodesContainer}>
+          <h2>promocodes:</h2>
+          <p className={styles.promocodes}>xxx-xxx-xxx-xxx</p>
+          <p className={styles.promocodes}>xxx-xxx-xxx-xxx</p>
+          <p className={styles.promocodes}>xxx-xxx-xxx-xxx</p>
         </div>
-        <div className={styles.imageContainer}>
-          <video
-            src="/indexVideo.mp4"
-            autoPlay
-            loop
-            muted
-            playsInline
-            className={styles.heroImage}
-          />
-        </div>
+      </div>
+      <div className={styles.imageContainer}>
+        <video
+          preload="none"
+          src="/indexVideo.mp4"
+          autoPlay
+          loop
+          muted
+          playsInline
+          className={styles.heroImage}
+        />
       </div>
     </section>
   );
