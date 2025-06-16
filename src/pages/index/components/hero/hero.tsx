bd1cd1fe9182b0d9da5185750ccbca47b7c3bc6c
wingedseraph <r89647@gmail.com>
2025-06-16 19:50:41 +0300

    feat: add description for promocodes index page

--- src/pages/index/components/hero/hero.tsx
+++ src/pages/index/components/hero/hero.tsx
@@ -21,8 +21,14 @@ export default function Hero() {
         </Link>
         <div className={styles.promocodesContainer}>
           <h2>promocodes:</h2>
-          <p className={styles.promocodes}>{PROMOCODES.SAVE20}</p>
-          <p className={styles.promocodes}>{PROMOCODES.RSSCHOOL10}</p>
+          <p className={styles.promocodes}>
+            Buy more then two items and get 20% -{' '}
+            <span className={styles.highlight}>{PROMOCODES.SAVE20}</span>
+          </p>
+          <p className={styles.promocodes}>
+            Discount 10% by RSSchool -{' '}
+            <span className={styles.highlight}>{PROMOCODES.RSSCHOOL10}</span>
+          </p>
         </div>
       </div>
       <div className={styles.imageContainer}>
