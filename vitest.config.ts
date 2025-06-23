498edeb349113251a8bf292fa02cea79fff04972
YuliAdam <ylia_adamovich@mail.ru>
2025-06-23 15:14:45 +0200

    feat: add notFound test file

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,15 @@
 import { defineConfig, mergeConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 import viteConfig from './vite.config';
+import { resolve } from 'node:path';
 
 export default mergeConfig(
   viteConfig,
   defineConfig({
     plugins: [react()],
+    resolve: {
+      alias: [{ find: '@', replacement: resolve(__dirname, './src') }],
+    },
     test: {
       environment: 'jsdom',
       globals: true,
